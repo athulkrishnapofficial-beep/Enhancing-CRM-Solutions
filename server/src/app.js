5810c4de4de32bf477219f03cf2613f37f31f6ee
athulkrishnap25 <athulkrishnapjio@gmail.com>
2025-12-02 12:08:03 +0530

    second

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,12 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import customerRoutes from "./routes/customerRoutes.js"; 
+import customerRoutes from "./routes/customerRoutes.js";
 
 const app = express();
 
 app.use(cors({
-  origin: "https://enhancing-crm-solutions.vercel.app/", 
+  origin: ["http://localhost:5173", "https://enhancing-crm-solutions.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials: true 
 }));
@@ -19,6 +19,5 @@ app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/admin", adminRoutes);
 app.use("/customers", customerRoutes);
-
 
 export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
-import dotenv from "dotenv";
-import app from "./app.js";
+import app from "./app.js"; 
 import { connectDB } from "./config/db.js";
-
-dotenv.config();
 
 connectDB();
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000 ✔");
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
